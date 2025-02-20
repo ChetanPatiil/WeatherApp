09343be67d49a65f1cbb2b352dfc4f66c027f4ee
ChetanPatiil <chetan.patil@svp.edu.in>
2025-02-20 10:02:17 +0530

    Update newsAPITest.js

--- android/newsAPITest.js
+++ android/newsAPITest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const getWeatherNews = async () => {
-    const url = `https://newsapi.org/v2/everything?q=weather&apiKey=749d60163cca4d6baafddd0d465cffd0`; 
+    const url = `https://newsapi.org/v2/everything?q=weather&apiKey=APIKEY`; 
 
     try {
       const response = await axios.get(url);
